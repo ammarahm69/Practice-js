3c78474df65088251b40eda8b36e7462c47a0467
Ammar Ahmed <ammar69aa59@gmail.com>
2024-06-03 12:26:38 -0700

    jh

--- app.js
+++ app.js
@@ -5,3 +5,16 @@
 // newNode.textContent = "Welcome"
 // parentDiv.insertBefore(newNode,child)
 // console.log(parentDiv);
+
+let parentDiv = document.getElementById('parentElement')
+// console.log(parentDiv);
+let firstChild = parentDiv.firstChild
+// Create a new element
+let newElement = document.createElement("h1");
+newElement.textContent = "Hello World"
+let newElement2 = document.createElement("h4");
+newElement2.textContent = "Hello"
+
+// Insert the new element before the first child
+parentDiv.insertBefore(newElement2 , firstChild);
+console.log(parentDiv);
