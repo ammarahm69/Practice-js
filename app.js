32149e009a2306c9f4972d613a8eed0655acc8ff
Ammar Ahmed <ammar69aa59@gmail.com>
2024-06-03 12:19:12 -0700

    sdf

---
+++ app.js
@@ -0,0 +1,7 @@
+// let newNode = document.createElement('h1')
+// let parentDiv = document.getElementById('childElement').parentNode
+// let child = document.getElementById('childElement')
+// child.textContent = "Hello Parat!"
+// newNode.textContent = "Welcome"
+// parentDiv.insertBefore(newNode,child)
+// console.log(parentDiv);
